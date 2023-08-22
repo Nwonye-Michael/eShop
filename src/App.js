06ecb45bdd0f29a3336d29ba97a8fa856dc225b0
Nwonye Michael <nwonye14@gmail.com>
2023-08-22 12:50:28 +0100

    Upload my React app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from "./logo.svg"
 import "./App.css"
-import Header from "./components/Header"
-import Home from "./pages/Home"
+// import Header from "./components/Header"
+// import Home from "./pages/Home"
 import Router from "./routes/Router"
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+// import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 
 function App() {
   return (
     <div className="App ">
-      <BrowserRouter>
-        {/* <Header /> */}
-        <Router />
-      </BrowserRouter>
+      {/* <Header /> */}
+      <Router />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { StateProvider } from "./state/StateProvider"
 import reducer, { initialState } from "./state/reducer"
+import { BrowserRouter } from "react-router-dom"
 import "./assets/fonts/TiltPrism-Regular.ttf"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
+    <BrowserRouter>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
 
